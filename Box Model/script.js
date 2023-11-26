ed007717c0f205c8087a7cc78c78a562cdb1716f
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-11-25 22:02:20 -0500

    Add Selectors

--- Box Model/script.js
+++ Box Model/script.js
@@ -1,1 +1,9 @@
+(function () {
+  const toggleSwitch = document.querySelector('.toggle__element');
+  const toggleLabel = document.querySelector('.toggle__label');
 
+  toggleSwitch.addEventListener('change', () => {
+    let state = toggleSwitch.checked ? 'ON' : 'OFF';
+    toggleLabel.innerHTML = state;
+  });
+})();
