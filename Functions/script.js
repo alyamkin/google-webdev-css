759ff971f2e4a43bca0030dd0628288667532ecb
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-12-28 10:31:36 -0500

    Add Functions

---
+++ Functions/script.js
@@ -0,0 +1,11 @@
+// const slider = document.querySelector('input[type=range]');
+// const box = document.querySelector('.box');
+
+// slider.addEventListener('input', () => {
+//   box.style.transform = `rotateZ(${slider.value}deg)`;
+//   box.style.transform = `rotate3d(1,1,1,${slider.value}deg)`;
+//   box.style.transform = `scale(${slider.value})`;
+//   box.style.transform = `translateZ(${slider.value}px)`;
+// });
+
+// Perspective
